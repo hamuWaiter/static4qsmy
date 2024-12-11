d9dce78412dcc15acbc267318cfbc287d53f1c5e
hamuWaiter <54763552+hamuWaiter@users.noreply.github.com>
2024-12-11 17:41:54 +0800

    feat: 静态文件服务add

--- src/modules/file/file.module.ts
+++ src/modules/file/file.module.ts
@@ -3,22 +3,33 @@
  * */
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 import { extname } from 'path';
 import { diskStorage } from 'multer';
 
 const storage = diskStorage({
   destination: './uploads',
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const ext = extname(file.originalname);
-    cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+    // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    // const ext = extname(file.originalname);
+    // cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+    cb(null, file.originalname);
   },
 });
+
 
 @Module({
   imports: [
     MulterModule.register({
       storage,
+    }),
+    // 静态文件服务
+    ServeStaticModule.forRoot({
+      // 目标目录
+      rootPath: './uploads',
+      // 访问静态文件的前缀
+      serveRoot: '/uploads',
     }),
   ],
   exports: [MulterModule],
